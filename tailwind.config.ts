3b49b8b0be473e6b9aab185b85678caa60c70ac0
Austin Chiatto <chiattoaustin@gmail.com>
2024-08-22 05:59:19 -0700

    implement custom tailwind and theme styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,52 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        'card-background': 'hsl(var(--card-background))',
+        'card-border': 'hsl(var(--card-border))',
+        'input-background': 'hsl(var(--input-background))',
+        muted: 'hsl(var(--muted))',
+        accent: 'hsl(var(--accent))',
+        border: 'hsl(var(--border))'
       },
-    },
+      textColor: {
+        foreground: 'hsl(var(--foreground))',
+        muted: 'hsl(var(--muted))'
+      },
+      backgroundColor: {
+        background: 'hsl(var(--background))',
+        'card-background': 'hsl(var(--card-background))',
+        'input-background': 'hsl(var(--input-background))',
+        muted: 'hsl(var(--muted))',
+        accent: 'hsl(var(--accent))'
+      },
+      borderColor: {
+        'card-border': 'hsl(var(--card-border))'
+      },
+      borderRadius: {
+        xl: '18px',
+        lg: '12px',
+        md: '8px',
+        sm: '6px'
+      },
+      fontSize: {
+        sm: '16px',
+        lg: '22px',
+        '7xl': '96px'
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
